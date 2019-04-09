3348c582ec46e9bcd4adc38c248c72054d9b52cb
LigiaTr <ligiatrisca@gmail.com>
2019-04-09 17:02:12 +0300

    Added documentation to the most important java classes

--- src/org/loose/fis/reservation/manager/Main.java
+++ src/org/loose/fis/reservation/manager/Main.java
@@ -26,3 +26,23 @@ public class Main {
         reservations.forEach(System.out::println);
     }
 }
+/**
+ * This is the main class of the reservation manager
+ * Currently this class contains some dummy users and some dummy reservations.
+ * Finally, after creating them, the main method of this class prints them out.
+ *
+ * Feel free to run the main method at any time. :)
+ */
+
+
+/**
+ * @param args not used
+ *
+ * this method starts the application :)
+ */
+
+
+//creating the users
+//creating reservations
+//adding all the reservations to a list
+//printing out the reservations
